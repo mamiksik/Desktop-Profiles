06bf37af19845d032a7624f0dc37842864a96c40
Martin Miksik <mamiksik@gmail.com>
2019-02-16 15:00:12 +0100

    Refactore workflow entity

--- Shared/Model/Entities/Workflow.swift
+++ Shared/Model/Entities/Workflow.swift
@@ -1,7 +1,19 @@
 import Foundation
 import RealmSwift
+import ShellOut
 
 final class Workflow: BaseEntity, Runable {
     @objc dynamic var name = ""
     @objc dynamic var path = ""
+}
+
+extension Workflow {
+    func run() throws {
+        let scriptQueue = DispatchQueue(label: CustomQueue.appleScript.rawValue)
+        let path = self.path.replacingOccurrences(of: " ", with: "\\ ")
+        
+        scriptQueue.async {
+            try! shellOut(to: "/usr/bin/automator", arguments: [path])
+        }
+    }
 }
